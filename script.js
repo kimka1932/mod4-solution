9098c7bf3357d29acee4eee086c496becfc93536
kimka1932 <kroom3912@gmail.com>
2024-08-21 00:11:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// STEP 1: Wrap the entire contents inside of an IIFE
+(function() {
+  // Define the names array
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // STEP 10: Loop over the names array
+  for (var i = 0; i < names.length; i++) {
+    // STEP 11: Retrieve the first letter of the current name
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // STEP 12: Compare the 'firstLetter' to 'j' and call the appropriate method
+    if (firstLetter === 'j') {
+      // Call byeSpeaker's 'speak' method
+      byeSpeaker.speak(names[i]);
+    } else {
+      // Call helloSpeaker's 'speak' method
+      helloSpeaker.speak(names[i]);
+    }
+  }
+})();
 
